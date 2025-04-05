4a2cc84b3c2fcdaa55140c7c662f19e769e4de7e
DeadAirX <djdivinity7@gmail.com>
2025-04-05 15:40:23 -0500

    add proper roll functionality

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 I want to be able to message a telegram bot `roll d6` and it responds with a result
 
+![](assets/demo.gif)
+
+# Setup
+`TELOXIDE_TOKEN` must be set to the Bot Token provided by @BotFather on Telegram
+
+if you don't have a bot set up, run `/newbot` in a chat with @BotFather to get
+a token
+
+
 # TODO
 - [x] Read up on Telegram API
 - [x] Get a bot account
 - [x] bot hello world
-- [ ] create a service that responds to a chat, by calling an cli command
+- [x] create a service that responds to a chat and generates a random number
+
+
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
 use teloxide::{prelude::Requester, types::Message, Bot};
+use rand::Rng;
+use pretty_env_logger;
+
+fn parse_dice_command(text: &str) -> Option<u32> {
+    let text = text.trim().to_lowercase();
+    if text.starts_with("roll d") {
+        // this feels brittle, but it works for now
+        text[6..].parse::<u32>().ok()
+    } else {
+        None
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -8,7 +20,14 @@ async fn main() {
     let bot = Bot::from_env();
 
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        bot.send_dice(msg.chat.id).await?;
+        if let Some(text) = msg.text() {
+            if let Some(sides) = parse_dice_command(text) {
+                let result = rand::thread_rng().gen_range(1..=sides);
+                if let Err(e) = bot.send_message(msg.chat.id, format!("🎲 Rolled a d{}: {}", sides, result)).await {
+                    log::error!("Failed to send message: {}", e);
+                }
+            }
+        }
         Ok(())
     })
     .await;
